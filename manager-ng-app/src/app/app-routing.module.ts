04b5aaddf87e78a6eb5c45f51f84c2fc9c3fde91
Anu Srikanth <anu.srikanth@outlook.com>
2019-02-23 21:54:10 -0500

    routing set up for guest, reservation routes. Reservation and Guest component created. Default and error page also set up

--- manager-ng-app/src/app/app-routing.module.ts
+++ manager-ng-app/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuestComponent } from './guest/guest.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'guests',
+  component: GuestComponent,
+},
+{
+  path: 'reservations',
+  component: ReservationComponent,
+},
+{
+  path:'',
+  component: GuestComponent,
+  pathMatch:'full'
+},
+{
+  path: '**',
+  component: NotFoundComponent,
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {enableTracing: true})
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- manager-ng-app/src/app/app.module.ts
+++ manager-ng-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { GuestComponent } from './guest/guest.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    GuestComponent,
+    NotFoundComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
