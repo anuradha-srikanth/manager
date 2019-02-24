6dd513560ea83ba1ead2843868f42787b8865cce
Anu Srikanth <anu.srikanth@outlook.com>
2019-02-24 11:35:07 -0500

    added GuestViewModel to further detach validations and business logic from backend model, exported ViewModel interface to set up databinding with Guest form:

--- manager-ng-app/src/app/guest/guest.component.ts
+++ manager-ng-app/src/app/guest/guest.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guest.component.css']
 })
 export class GuestComponent implements OnInit {
+  model: GuestViewModel = {
+    firstname: '',
+    last_name: '';
+    address: '';
+    city: '';
+    state: '';
+    zip: '';
+    phone: '';
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+
+export interface GuestViewModel{
+  first_name:string;
+  last_name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  phone: string;
 }
